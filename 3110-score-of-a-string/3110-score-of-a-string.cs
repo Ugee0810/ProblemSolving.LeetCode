a52ad4287d98696d400a4178bd5f5f471990510d
송환욱(Song HwanUk) <85896566+Ugee0810@users.noreply.github.com>
2024-08-01 16:19:52 +0900

    Time: 45 ms (63.39%), Space: 38.6 MB (72.12%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cs
@@ -0,0 +1,12 @@
+public class Solution
+{
+    public int ScoreOfString(string s)
+    {
+        var score = 0;
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            score += Math.Abs(s[i] - s[i + 1]);
+        }
+        return score;
+    }
+}
